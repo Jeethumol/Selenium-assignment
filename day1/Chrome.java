f131e27b832035140175c91eea0e2a9abac07625
Jeethumol <jeethuloussy@gmail.com>
2025-04-26 16:18:42 -0400

    Add files via upload
    Week 1- day1- Home Assignment

---
+++ day1/Chrome.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float version=91.0f;
+System.out.println(version);
+
+String Developer="Google";
+System.out.println(Developer);
+
+boolean isBeta=false;
+System.out.println(isBeta);
+
+int releaseYear=2008;
+System.out.println(releaseYear);
+
+char shortcutKey='C';
+System.out.println(shortcutKey);
+
+	}
+
+}
